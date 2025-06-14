e95bcb09e41c30864a2a5d550f7b295322f1e589
nipunbatra8 <83050981+nipunbatra8@users.noreply.github.com>
2025-06-14 08:27:05 -0700

    fix imports

--- src/pages/lawyer/index.ts
+++ src/pages/lawyer/index.ts
@@ -2,4 +2,4 @@ export { default as DashboardPage } from './DashboardPage';
 export { default as ClientsPage } from './Clients';
 export { default as SettingsPage } from './SettingsPage';
 export { default as CasePage } from './CasePage';
-export { default as ArchivePage } from './ArchivePage'; 
+export { default as ArchivePage } from './ArchivePage';
